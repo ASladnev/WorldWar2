ed45c5ec94d219620b2bb0febe0a701dc51f837c
ASladnev <ASladnev@gmail.com>
2015-05-04 13:12:51 +0300

    Ng View

--- JS/ww2Application.js
+++ JS/ww2Application.js
@@ -1,4 +1,43 @@
 /**
  * Created by user on 03.05.15.
  */
-angular.module ("ww2Application", ['ngRoute']);
+angular.module ("ww2Application", ['ngRoute'])
+
+    .config(['$routeProvider', '$locationProvider',
+        function($routeProvider, $locationProvider) {
+
+            $routeProvider
+                .when ('/', {
+                    templateUrl: 'Main.html'
+                })
+                .when('/MoscowBattle', {
+                    templateUrl: 'Battle/Moscow.html'//,
+                    //controller: 'MainCtrl',
+                    //controllerAs: 'main'
+                })
+                .when('/StalingradBattle', {
+                    templateUrl: 'Battle/Stalingrad.html'
+                    //,controller: 'ChapterCtrl',
+                    //controllerAs: 'chapter'
+                })
+                .when('/KurskBattle', {
+                    templateUrl: 'Battle/Kursk.html'
+                    //,controller: 'ChapterCtrl',
+                    //controllerAs: 'chapter'
+                });
+
+
+            ;
+
+//            $locationProvider.html5Mode(true);
+        }])
+
+    .controller('MainCtrl', ['$route', '$routeParams', '$location',
+        function($route, $routeParams, $location) {
+            this.$route = $route;
+            this.$location = $location;
+            this.$routeParams = $routeParams;
+        }])
+
+        ;
+

b06e3a02610e6d40497c9435e5010303cef2e978
ASladnev <ASladnev@gmail.com>
2015-05-05 14:03:29 +0300

    Moscow image

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,33 +1,36 @@
  //* Created by 805159 on 04.05.2015.
 
  $(function () {
-     var x;
-     console.log ("DOM is ready");
-     $("#p2").hide();
-     $("#p3").hide();
-     var w = 0;
-      $("#poemButton").on ('click', function () {
-          if (w > 2) w = 0;
-          if (w == 0) {
-              $("#p1").hide();
-              $("#p3").hide();
-             // $("#p2").show ();
-              $("#p2").slideDown(3000);
-          }
-          if (w == 1) {
-              $("#p2").hide();
-              $("#p1").hide();
-             // $("#p3").show ();
-              $("#p3").slideDown(3000);
-          }
-          if (w == 2) {
-              $("#p2").hide();
-              $("#p3").hide();
-             // $("#p1").show ();
-              $("#p1").slideDown(3000);
-          }
-          w++;
-      });
+
+    var x;
+    console.log ("DOM is ready");
+    $("#p2").hide();
+    $("#p3").hide();
+    var w = 0;
+    $("#poemButton").on ('click', function () {
+      if (w > 2) w = 0;
+      if (w == 0) {
+          $("#p1").hide();
+          $("#p3").hide();
+         // $("#p2").show ();
+          $("#p2").slideDown(3000);
+      }
+      if (w == 1) {
+          $("#p2").hide();
+          $("#p1").hide();
+         // $("#p3").show ();
+          $("#p3").slideDown(3000);
+      }
+      if (w == 2) {
+          $("#p2").hide();
+          $("#p3").hide();
+         // $("#p1").show ();
+          $("#p1").slideDown(3000);
+      }
+      w++;
+     });
+
+
 
      var im1 = "<img src='img/im1.jpg' width='470px' height='378px'/>";
      var im2 = "<img src='img/im2.jpg' width='470px' height='378px'/>";
@@ -43,6 +46,7 @@
      });
 
      function loadImage () {
+         if (document.getElementById("span") == null) return;
          if (v > 6) v = 0;
          if (v == 0) document.getElementById("span").innerHTML = im1;
          if (v == 1) document.getElementById("span").innerHTML = im2;
@@ -58,6 +62,7 @@
       setInterval(Image, x);
       });
       */
+
      setInterval(loadImage, 5000);
 
 

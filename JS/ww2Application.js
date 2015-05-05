5303ad01d167fb1e8994655b9ecdafb2b4acf322
ASladnev <ASladnev@gmail.com>
2015-05-05 16:22:32 +0300

    Moscow image && jquery

--- JS/ww2Application.js
+++ JS/ww2Application.js
@@ -1,29 +1,16 @@
 /**
  * Created by user on 03.05.15.
  */
-angular.module ("ww2Application", ['ngRoute'])
+angular.module ("ww2Application", ['ngRoute', 'ngAnimate'])
 
     .config(['$routeProvider', '$locationProvider',
         function($routeProvider, $locationProvider) {
             $routeProvider
-                .when ('/', {
-                    templateUrl: 'Main.html'
-                })
-                .when('/MoscowBattle', {
-                    templateUrl: 'Battle/Moscow.html'//,
-                    //controller: 'MainCtrl',
-                    //controllerAs: 'main'
-                })
-                .when('/StalingradBattle', {
-                    templateUrl: 'Battle/Stalingrad.html'
-                    //,controller: 'ChapterCtrl',
-                    //controllerAs: 'chapter'
-                })
-                .when('/KurskBattle', {
-                    templateUrl: 'Battle/Kursk.html'
-                    //,controller: 'ChapterCtrl',
-                    //controllerAs: 'chapter'
-                });
+                .when ('/', {templateUrl: 'Main.html'})
+                .when ('/Battles', {templateUrl: 'Battle/Battles.html'})
+                .when('/MoscowBattle', {templateUrl: 'Battle/Moscow.html'})
+                .when('/StalingradBattle', {templateUrl: 'Battle/Stalingrad.html'})
+                .when('/KurskBattle', {templateUrl: 'Battle/Kursk.html'});
 
 
 //            $locationProvider.html5Mode(true);
@@ -34,7 +21,5 @@ angular.module ("ww2Application", ['ngRoute'])
           //  this.$route = $route;
           //  this.$location = $location;
           //  this.$routeParams = $routeParams;
-        }])
-
-        ;
+        }]) ;
 

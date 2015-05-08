a3a42ae806e3ec5b0aff9d94fddc37eba73e2eb5
ASladnev <ASladnev@gmail.com>
2015-05-08 13:57:29 +0300

    Fixed Poems && Moscow

--- JS/Moscow.js
+++ JS/Moscow.js
@@ -2,50 +2,31 @@
  * Created by 805159 on 05.05.2015.
  */
 $(function () {
+    console.log ("moscow.js is loaded");
 
-    /*$(".im").on ('mouseover', function () {
-        //   $(".im").css({
-        //      width: 200
-        //    });
-        alert("Hello world");
-    });
+    var arr = ["<img src='Moscow/Moscow1.jpg' height='150px' width='200px'>",
+        "<img src='Moscow/Moscow2.jpg' height='150px' width='200px'>",
+        "<img src='Moscow/Moscow3.jpg' height='150px' width='200px'>",
+        "<img src='Moscow/Moscow4.jpg' height='150px' width='200px'>",
+        "<img src='Moscow/Moscow5.jpg' height='150px' width='200px'>",
+        "<img src='Moscow/Moscow6.jpg' height='150px' width='200px'>"];
 
-    $(".im").hover(function () {
-        $(this).css ("width", "200px").css("height", "200px");
-    });
-    */
-    var arr = ["<img src='Moscow/Moscow1.jpg' height='150px' width='180px'>",
-        "<img src='Moscow/Moscow2.jpg' height='150px' width='180px'>",
-        "<img src='Moscow/Moscow3.jpg' height='150px' width='180px'>",
-        "<img src='Moscow/Moscow4.jpg' height='150px' width='180px'>",
-        "<img src='Moscow/Moscow5.jpg' height='150px' width='180px'>",
-        "<img src='Moscow/Moscow6.jpg' height='150px' width='180px'>"];
-    document.getElementById("imagexxx").innerHTML = arr[0];
-    var v = 1;
-    $('body').on ('load', function () {
-       Change();
-    });
+    //document.getElementById("imagexxx").innerHTML = arr[0];
+
+    var v = 0;
+
+    Change();
+
     function Change () {
         if (v > 5) v = 0;
-        document.getElementById("imagexxx").innerHTML = arr[v];
-        //if (v == 0) document.getElementById("image").innerHTML = arr[0];
-        //if (v == 1) document.getElementById("image").innerHTML = arr[1];
-        //if (v == 2) document.getElementById("image").innerHTML = arr[2];
-        //if (v == 3) document.getElementById("image").innerHTML = arr[3];
-        //if (v == 4) document.getElementById("image").innerHTML = arr[4];
-        //if (v == 5) document.getElementById("image").innerHTML = arr[5];
-        v++;
-    }
-    var x;
-    function Num () {
-        alert("aa");
+        var imageDiv = document.getElementById ("imageDiv");
+        if (imageDiv == null) {
+            clearInterval(interval);
+            return;
+        }
+        imageDiv.innerHTML = arr[v++];
     }
 
-    $("#imagexxx").on ('click', function () {
-        Num();
-    });
-
-        setInterval(Change, 5000);
-
+    var interval = setInterval(Change, 5000);
 
 });

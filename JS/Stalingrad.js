67e61f98c962c7ed1ac86988e542846237847acd
ASladnev <ASladnev@gmail.com>
2015-05-12 12:08:02 +0300

    Modal & Carousel

--- JS/Stalingrad.js
+++ JS/Stalingrad.js
@@ -1,21 +1,31 @@
 /**
  * Created by 805159 on 07.05.2015.
  */
-var con = 200;
-var images = ["<img src='Stalingrad/Stalingrad1.jpg' height='150px' width='180px'>",
-    "<img src='Stalingrad/Stalingrad2.jpg' height='150px' width='200px'>",
-    "<img src='Stalingrad/Stalingrad3.jpg' height='150px' width='200px'>",
-    "<img src='Stalingrad/Stalingrad4.jpg' height='150px' width='200px'>",
-    "<img src='Stalingrad/Stalingrad5.jpg' height='150px' width='200px'>",
-    "<img src='Stalingrad/Stalingrad6.jpg' height='150px' width='200px'>"];
-document.getElementById("imageS").innerHTML = images[0];
-var x = 1;
-function Image () {
-    if (x > 5) x = 0;
-    document.getElementById("imageS").innerHTML = images[x];
-    x++;
-}
-$('body').on('load', function () {
-   Image();
+
+$(function () {
+    var con = 200;
+    var images = ["<img src='Stalingrad/Stalingrad1.jpg' height='150px' width='180px'>",
+        "<img src='Stalingrad/Stalingrad2.jpg' height='150px' width='200px'>",
+        "<img src='Stalingrad/Stalingrad3.jpg' height='150px' width='200px'>",
+        "<img src='Stalingrad/Stalingrad4.jpg' height='150px' width='200px'>",
+        "<img src='Stalingrad/Stalingrad5.jpg' height='150px' width='200px'>",
+        "<img src='Stalingrad/Stalingrad6.jpg' height='150px' width='200px'>"];
+    document.getElementById("imageS").innerHTML = images[0];
+
+    var x = 1;
+    function Image() {
+        if (x > 5) x = 0;
+        var imageS = document. getElementById("imageS");
+        if (imageS == null) {
+            clearInterval(interval);
+            return;
+        };
+        imageS.innerHTML = images[x];
+        x++;
+    }
+
+    $('body').on('load', function () {
+        Image();
+    });
+    var interval = setInterval(Image, 5000);
 });
-setInterval(Image, 5000);
